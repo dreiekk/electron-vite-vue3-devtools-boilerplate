1741fa4357306762311450f5d83535abadd72717
dreiekk <dreiekk@users.noreply.github.com>
2022-05-10 22:17:20 +0200

    Fix: connect devtools on startup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import devtools from '@vue/devtools'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if (process.env.NODE_ENV === 'development') {
+    devtools.connect()
+}
